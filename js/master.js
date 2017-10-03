b01b368394eb48de8b93d6e790f6353931438e38
lchaconc <luis.chacon.campos@gmail.com>
2017-10-03 12:55:44 -0600

    copia de los archivos fuente
    archivos del web app

---
+++ js/master.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+  $('#myModal').modal('hide');
+  $.getJSON("data/datos.json", function(datos) {
+    $(".table-responsive").click(function (e) {
+       var ident = e.target.id;
+      //  console.log(e.currentTarget);
+      //    $(e.currentTarget.td).css("background-color", "yellow");
+       cargarModal(ident.slice(0,3)); //Esto para trabajar con la misma información sin repetir ID
+    });
+    $(".headCalendar").click(collapseGlip);
+
+    function cargarModal(idCelda) {
+        var returnedData = $.grep(datos, function (element, index) {
+            return element.id == idCelda;
+        });
+        if (returnedData[0]){
+          $('#myModal').modal('show');
+          // $('#divID').css("background-image", "url(/myimage.jpg)");
+          // $('#myModal').css('background-image','url(image/fondo-dia1.png) repeat');
+          $('#myModal').on('show.bs.modal', function (event) {var modal = $(this)
+              modal.find('.modal-title').text(returnedData[0].taller);
+              var texto = "<p><strong>Institución:</strong> "+returnedData[0].institucion+"</p>"+
+                          "<p><strong>Descripción:</strong> "+returnedData[0].descripcion+"</p>"+
+                          "<p><strong>Audiencia:</strong> "+returnedData[0].audiencia+"</p>"+
+                          "<p><strong>Requisitos:</strong> "+returnedData[0].requisitos+"</p>"+
+                          "<p><strong>Horario:</strong> "+returnedData[0].horario+"</p>"+
+                          "<p><strong>Cantidad de participantes:</strong> "+returnedData[0].cantidad+"</p>"+
+                          "<p><strong>Observaciones:</strong> "+returnedData[0].observaciones+"</p>";
+              // if(returnedData[0].adjuntos !="")
+              //   {texto = texto+"<p><strong>Adjuntos:</strong> "+returnedData[0].adjuntos+"</p>";}
+              modal.find('.modal-body').html(texto);
+          });
+          $('#myModal').modal('show');
+        };
+    }
+  });
+});
+
+function collapseGlip() {
+  elementSpan = this.children;
+  $(".collapse").on('shown.bs.collapse', function(){
+      // The collapsible content is now fully shown.');
+      $(elementSpan).removeClass("glyphicon-plus");
+      $(elementSpan).addClass("glyphicon-minus");
+  });
+  $(".collapse").on('hidden.bs.collapse', function(){
+      // 'The collapsible content is about to be hidden
+      $(elementSpan).removeClass("glyphicon-minus");
+      $(elementSpan).addClass("glyphicon-plus");
+  });
+}
